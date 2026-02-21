161c2d68dc8f0e159c76ee1134581e85f1942a6c
Reve <payaco02@gmail.com>
2026-02-21 14:33:31 +0100

    Delete electron directory

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  getAppVersion: () => ipcRenderer.invoke("get-app-version"),
-  openFileDialog: () => ipcRenderer.invoke("open-file-dialog"),
-  openFolderDialog: () => ipcRenderer.invoke("open-folder-dialog"),
-  isElectron: true,
-});
